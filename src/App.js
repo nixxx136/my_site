3bc764cab7406045c437ba195c59d332527b3fa3
Penghuan Ni <nipeng@amazon.com>
2018-05-10 15:28:31 -0700

    add headers and some npm packages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Footer from './components/layout/Footer'
-import NavBar from './components/layout/NavBar'
+import Header from './components/layout/Header'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar />
+        <Header />
 
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-        </header>
+        </header> */}
         {/* <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p> */}
---
+++ src/components/layout/Header.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink } from 'reactstrap';
+
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">Penghuan Ni</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+
+            <Nav className="ml-auto" navbar>
+
+              <NavItem>
+                <NavLink href="#">About Me</NavLink>
+              </NavItem>
+
+              <NavItem>
+                <NavLink href="#">Projects</NavLink>
+              </NavItem>
+
+              <NavItem>
+                <NavLink href="#">Contact</NavLink>
+              </NavItem>
+
+            </Nav>
+
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
