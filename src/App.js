69143cb9131ed113fec3149919aac20b0b6a3013
Penghuan Ni <nipeng@amazon.com>
2018-06-04 23:39:28 -0700

    add some text

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import Footer from './components/layout/Footer'
 import Header from './components/layout/Header'
+import Home from './components/Home'
 import './App.css';
 
 class App extends Component {
@@ -9,15 +10,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header> */}
-        {/* <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p> */}
-
+        <Home />
         <div className="Footer"> <Footer /> </div>
       </div>
     );
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        Welcom to my page (under construction!)
+      </div>
+    );
+  }
+}
